c6881d2868cd8d1b6eba52e9307da276f85f0a79
Miles Bardon <milesbardon@gmail.com>
2019-12-23 18:09:51 +0000

    Updated signed url api

--- src/api/storage/index.js
+++ src/api/storage/index.js
@@ -26,40 +26,23 @@ export const submitImage = async file => {
     })
     .then(response => {
       console.log(response)
-      axios
-        .put(response.data.signed_url, file, {
-          headers: {
-            "Content-Type": file.type
-          }
-        })
-        .then(response => {
-          console.log("success : ", response);
-        })
-        .catch(error => {
-          console.log("errorSubmitImage : ", error);
-        });
+      const options = {
+        headers: {"x-goog-resumable": "start"}
+      };
+      axios.post(response.data.signed_url, {}, options)
+      .then(axiosResponse => {     // make sure axiosResponse.status = 201
+          axios.put(axiosResponse.headers.location, "file", { headers: { "Content-Type": file.type, "Content-Length": file.length} }).then(postResponse => {
+            console.log("success : ", postResponse);
+          }).catch(error => {
+            console.log("put : ", error);
+          });
+      }).catch(error => {
+        console.log("post : ", error);
+        return "";
+      }).catch(err => console.warn(err));
     })
     .catch(error => {
       console.log("errorSignedURL : ", error);
       return "";
     });
 };
-
-// const getSignedURL = async filename => {
-//   const url =
-//     "https://us-east1-yugiohbot.cloudfunctions.net/yugiohbot_signed-url";
-
-//   await axios
-//     .post(url, {
-//       bucket: "yugiohbot-images/pending",
-//       filename,
-//       contentType: "image/jpeg"
-//     })
-//     .then(response => {
-//       return response.data.signed_url;
-//     })
-//     .catch(error => {
-//       console.log("errorSignedURL : ", error);
-//       return "";
-//     });
-// };
