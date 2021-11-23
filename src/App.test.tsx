68739a558d07cc0b0a08cb224d9cab82de52f091
Miles Bardon <milesbardon@gmail.com>
2021-11-23 20:41:40 +0000

    Updating tests with better practises

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,26 +1,20 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from "@testing-library/react";
 
-describe('App', () => {
-  let App: () => JSX.Element;
-  const mockPreview = () => <div>Preview</div>;
-  const mockForm = () => <div>Submission Form</div>;
+import App from "./App";
 
-  beforeEach(() => {
-    jest.mock('./Preview', () => ({
-      Preview: mockPreview,
-    }));
-    jest.mock('./Form', () => ({
-      SubmissionForm: mockForm,
-    }));
+jest.mock("./Preview", () => ({
+  Preview: () => <div>Preview</div>,
+}));
+jest.mock("./Form", () => ({
+  SubmissionForm: () => <div>Submission Form</div>,
+}));
 
-    App = require('./App').default;
-  });
-
-  it('renders App', async () => {
+describe("App", () => {
+  it("renders App", async () => {
     render(<App />);
-    const header = await screen.findByAltText('Yu-Gi-Oh Bot 3000 logo');
-    const form = await screen.findByText('Submission Form');
-    const preview = await screen.findByText('Preview');
+    const header = await screen.findByAltText("Yu-Gi-Oh Bot 3000 logo");
+    const form = await screen.findByText("Submission Form");
+    const preview = await screen.findByText("Preview");
 
     expect(header).toBeInTheDocument();
     expect(form).toBeInTheDocument();
--- src/Form.test.tsx
+++ src/Form.test.tsx
@@ -1,93 +1,89 @@
-import { fireEvent, render, screen, act } from '@testing-library/react';
+import { fireEvent, render, screen, act } from "@testing-library/react";
+import mockAxios from "jest-mock-axios";
 
-describe('Submission Form', () => {
-  let Form: ({ setPreview }: { setPreview: () => void }) => JSX.Element;
-  const mockAxios = {
-    post: jest.fn(),
-  };
-  jest.spyOn(console, 'error').mockImplementation(() => {});
+import { SubmissionForm } from "./Form";
 
-  beforeEach(() => {
+describe("Submission Form", () => {
+  jest.spyOn(console, "error").mockImplementation(() => {});
+
+  afterEach(() => {
     jest.clearAllMocks();
-
-    jest.mock('axios', () => mockAxios);
-
-    Form = require('./Form').SubmissionForm;
+    mockAxios.reset();
   });
 
-  it('should render the form', async () => {
+  it("should render the form", async () => {
     const mockSetPreview = jest.fn();
-    render(<Form setPreview={mockSetPreview} />);
+    render(<SubmissionForm setPreview={mockSetPreview} />);
 
-    expect(await screen.findByText('?')).toBeInTheDocument();
-    expect(await screen.findByLabelText('Upload Image')).toBeInTheDocument();
-    expect(await screen.findByText('✔')).toBeInTheDocument();
+    expect(await screen.findByText("?")).toBeInTheDocument();
+    expect(await screen.findByLabelText("Upload Image")).toBeInTheDocument();
+    expect(await screen.findByText("✔")).toBeInTheDocument();
   });
 
-  describe('given the axios request succeeds', () => {
+  describe("given the axios request succeeds", () => {
     beforeEach(() => {
       mockAxios.post.mockResolvedValue(true);
       global.URL.createObjectURL = jest.fn((file: File) => file.name);
     });
 
-    it('should POST the submitted image', async () => {
+    it("should POST the submitted image", async () => {
       const mockSetPreview = jest.fn();
-      render(<Form setPreview={mockSetPreview} />);
+      render(<SubmissionForm setPreview={mockSetPreview} />);
 
-      const file = new File(['(⌐□_□)'], 'chucknorris.png', {
-        type: 'image/png',
+      const file = new File(["(⌐□_□)"], "chucknorris.png", {
+        type: "image/png",
       });
-      const imageInput = await screen.findByLabelText('Upload Image');
-      const submit = await screen.findByText('✔');
+      const imageInput = await screen.findByLabelText("Upload Image");
+      const submit = await screen.findByText("✔");
 
-      Object.defineProperty(imageInput, 'files', { value: [file] });
+      Object.defineProperty(imageInput, "files", { value: [file] });
       fireEvent.change(imageInput);
-      expect(mockSetPreview).toBeCalledWith('chucknorris.png');
+      expect(mockSetPreview).toBeCalledWith("chucknorris.png");
 
       await act(async () => {
         fireEvent.click(submit);
       });
 
       expect(mockAxios.post).toBeCalledWith(
-        'https://us-east1-yugiohbot.cloudfunctions.net/yugiohbot_submission',
+        "https://us-east1-yugiohbot.cloudfunctions.net/yugiohbot_submission",
         expect.any(FormData),
         {}
       );
 
-      const message = await screen.findByTestId('message');
+      const message = await screen.findByTestId("message");
       expect(message.innerHTML).toBe(
-        'Successfully uploaded your photo: chucknorris.png'
+        "Successfully uploaded your photo: chucknorris.png"
       );
     });
   });
 
-  describe('given the axios request fails', () => {
+  describe("given the axios request fails", () => {
     beforeEach(() => {
       mockAxios.post.mockRejectedValue(false);
       global.URL.createObjectURL = jest.fn((file: File) => file.name);
     });
 
-    it('should report a failure to the user', async () => {
+    it("should report a failure to the user", async () => {
       const mockSetPreview = jest.fn();
-      render(<Form setPreview={mockSetPreview} />);
+      render(<SubmissionForm setPreview={mockSetPreview} />);
 
-      const file = new File(['(⌐□_□)'], 'chucknorris.png', {
-        type: 'image/png',
+      const file = new File(["(⌐□_□)"], "chucknorris.png", {
+        type: "image/png",
       });
-      const imageInput = await screen.findByLabelText('Upload Image');
-      const submit = await screen.findByText('✔');
+      const imageInput = await screen.findByLabelText("Upload Image");
+      const submit = await screen.findByText("✔");
 
-      Object.defineProperty(imageInput, 'files', { value: [file] });
+      Object.defineProperty(imageInput, "files", { value: [file] });
       fireEvent.change(imageInput);
-      expect(mockSetPreview).toBeCalledWith('chucknorris.png');
+      expect(mockSetPreview).toBeCalledWith("chucknorris.png");
 
       await act(async () => {
         fireEvent.click(submit);
       });
 
-      const message = await screen.findByTestId('message');
+      const message = await screen.findByTestId("message");
       expect(message.innerHTML).toBe(
-        'Something went wrong, please try again later.'
+        "Something went wrong, please try again later."
       );
     });
   });
