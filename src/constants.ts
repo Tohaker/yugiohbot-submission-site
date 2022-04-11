1bb6e78a6b11130a67433dfd69690845fa17577d
Miles Bardon <milesbardon@gmail.com>
2022-04-11 22:04:11 +0100

    Updating submission URL

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 export const SUBMISSION_URL =
-  "https://937pdhdob5.execute-api.eu-central-1.amazonaws.com/prod/submission";
+  "https://2ghq6ndg5b3ysujg5dzv2qcuy40lkznh.lambda-url.eu-central-1.on.aws/";
