58a51c5c56ddf08ef41035a7157718e480de4549
Miles Bardon <milesbardon@gmail.com>
2022-04-16 10:02:23 +0100

    Removing logo and preview from App

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,22 +2,13 @@ import { render, screen } from "@testing-library/react";
 
 import App from "./App";
 
-jest.mock("./Preview", () => ({
-  Preview: () => <div>Preview</div>,
-}));
 jest.mock("./Form", () => ({
   SubmissionForm: () => <div>Submission Form</div>,
 }));
 
 describe("App", () => {
-  it("renders App", async () => {
+  it("renders App", () => {
     render(<App />);
-    const header = await screen.findByAltText("Yu-Gi-Oh Bot 3000 logo");
-    const form = await screen.findByText("Submission Form");
-    const preview = await screen.findByText("Preview");
-
-    expect(header).toBeInTheDocument();
-    expect(form).toBeInTheDocument();
-    expect(preview).toBeInTheDocument();
+    expect(screen.getByText("Submission Form")).toBeInTheDocument();
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
-import { useState } from 'react';
-import { Preview } from './Preview';
-import { SubmissionForm } from './Form';
-import headerImage from './assets/logo-trans.png';
-import { Container, HeaderImage, Page } from './App.styles';
+import { SubmissionForm } from "./Form";
+import { Container } from "./App.styles";
 
-const App = () => {
-  const [preview, setPreview] = useState('');
-
-  return (
-    <Container>
-      <HeaderImage src={headerImage} alt="Yu-Gi-Oh Bot 3000 logo" />
-      <Page>
-        <SubmissionForm setPreview={setPreview} />
-        <Preview image={preview} />
-      </Page>
-    </Container>
-  );
-};
+const App = () => (
+  <Container>
+    <SubmissionForm />
+  </Container>
+);
 
 export default App;
